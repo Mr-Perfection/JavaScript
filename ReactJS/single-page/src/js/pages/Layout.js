394209aa1b3928298130bebaf1e54aa87448d6e2
Sungsoo lee <sungsoolee0127@gmail.com>
2017-01-24 21:14:16 -0800

    REACT JS TUTORIAL #6 - React Router & Intro to Single Page Apps with React JS

---
+++ ReactJS/single-page/src/js/pages/Layout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router";
+
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+
+export default class Layout extends React.Component {
+  navigate() {
+    // this.props.history.pushState(null, "/");
+    this.props.history.replaceState(null, "/");
+    console.log(this.props);
+  }
+  render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+    console.log("layout");
+    return (
+      <div>
+        <Nav location={location} />
+        <div class="container medium" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>Stephenslee.net</h1>
+              <Link to="archives"><button class="btn btn-primary">archives</button></Link>
+              <Link to="settings"><button class="btn btn-success">settings</button></Link>
+              <button onClick={this.navigate.bind(this)}>featured</button>
+              {this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
+      </div>
+
+    );
+  }
+}
