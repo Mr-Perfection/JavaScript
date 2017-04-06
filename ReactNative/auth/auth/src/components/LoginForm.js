449ea51105c1dcade9e7e8450e0abeb06f073343
Sungsoo lee <sungsoolee0127@gmail.com>
2017-04-06 12:50:33 -0700

    authentication with firebase: login-form is partly completed.

--- ReactNative/auth/auth/src/components/LoginForm.js
+++ ReactNative/auth/auth/src/components/LoginForm.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '' }
+  state = { email: '', password: '' };
   render() {
     return (
       <Card>
         <CardSection>
           <Input
             label="Email"
-            value={this.state.email}
             onChangeText={email => this.setState({ email })}
             placeholder="user@gmail.com"
           />
         </CardSection>
+        <CardSection>
+          <Input
+            placeholder="password"
+            label="Password"
+            onChangeText={password => this.setState({ password })}
+            secureTextEntry
+          />
 
-        <CardSection />
-
+        </CardSection>
         <CardSection>
           <Button>
             Log in
